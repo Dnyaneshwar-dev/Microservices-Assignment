1e1618d21754c367d1654e436faedc397831517d
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-01-19 11:56:55 +0530

    Added schema and  csv file middleware

---
+++ content-service/schema/content.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+const contentSchema = new mongoose.Schema({
+  contentid: {
+    type: String,
+    default: function genUUID() {
+      return uuidv4();
+    },
+  },
+  published_date: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  userid: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  title: {
+    type: String,
+    minlength: [1, "Title couldn't be empty"],
+  },
+
+  story: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+});
+
+module.exports = { contentSchema };
--- content-service/server.js
+++ content-service/server.js
@@ -1,8 +1,28 @@
-const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const csv = require("csvtojson");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename(req, file, cb) {
+    cb(null, `contents.csv`);
+  },
+});
+const upload = multer({ storage: storage });
+
+const { contentSchema } = require("./schema/content");
+
+const db = mongoose.createConnection(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(express.json());
 
@@ -10,23 +30,55 @@ const PORT = process.env.PORT || 5000;
 
 app.post("/books/new", async (req, res) => {
   const { title, story, userid, published_date } = req.body;
+
+  const Content = db.model("content", contentSchema);
   try {
-    const data = await prisma.content.create({
-      data: {
-        title: title,
-        story: story,
-        published_date: published_date,
-        userid: userid,
-      },
+    const data = new Content({
+      title: title,
+      story: story,
+      userid: userid,
+      published_date: published_date,
     });
+    const result = await data.save();
     res.json({
       ok: true,
-      message: "Content created successfully",
+      message: "Content added successfully",
     });
   } catch (error) {
     res.json({
       ok: false,
-      message: "Something Went Wrong",
+      message: error,
+    });
+  }
+});
+
+app.post("/books/newupload", upload.single("file"), async (req, res) => {
+  console.log(req.file);
+  const { title, story, userid, published_date } = req.body;
+
+  const filePath = path.join(__dirname, "/uploads/contents.csv");
+
+  const jsonData = await csv().fromFile(filePath);
+
+  const Content = db.model("content", contentSchema);
+  try {
+    for (let index = 0; index < jsonData.length; index++) {
+      const data = new Content({
+        title: title,
+        story: story,
+        userid: userid,
+        published_date: published_date,
+      });
+      const result = await data.save();
+    }
+    res.json({
+      ok: true,
+      message: "Content added successfully",
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      message: error,
     });
   }
 });

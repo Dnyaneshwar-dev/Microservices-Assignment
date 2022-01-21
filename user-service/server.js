7f43c21144a0cbbb3de9419393459894daa11739
Dnyaneshwar-dev <dnyaneshwar.ware@walchandsangli.ac.in>
2022-01-21 12:44:39 +0530

    Added user operations

--- user-service/server.js
+++ user-service/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const { useSchema } = require("./schema/user");
+const cookieParser = require("cookie-parser");
+const { useSchema } = require("./schema/user.js");
+
 const app = express();
 const db = mongoose.createConnection(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -10,8 +12,9 @@ const db = mongoose.createConnection(process.env.DATABASE_URL, {
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
-const PORT = process.env.DATABASE_URL || 7000;
+const PORT = process.env.PORT || 7000;
 
 app.post("/users/new", async (req, res) => {
   const { firstname, lastname, email, phone, password } = req.body;
@@ -24,21 +27,22 @@ app.post("/users/new", async (req, res) => {
         ok: false,
         message: "User with given email already exists",
       });
+      return;
+    } else {
+      const newUser = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phone: phone,
+        password: password,
+      });
+
+      await newUser.save();
+      res.send({
+        ok: true,
+        message: "User created successfully",
+      });
     }
-
-    const newUser = new User({
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-      phone: phone,
-      password: password,
-    });
-
-    await newUser.save();
-    res.send({
-      ok: true,
-      message: "User created successfully",
-    });
   } catch (error) {
     res.send({
       ok: false,
@@ -58,13 +62,15 @@ app.put("/users/update", async (req, res) => {
         ok: false,
         message: "User with given email doesn't exist",
       });
+    } else {
+      await User.updateOne({ email: userData.email }, userData);
+      res.send({
+        ok: true,
+        message: "User data updated successfully",
+      });
     }
-    await User.updateOne({ email: email }, userData);
-    res.send({
-      ok: true,
-      message: "User data updated successfully",
-    });
   } catch (error) {
+    console.log(error);
     res.send({
       ok: false,
       error: error,
@@ -73,26 +79,32 @@ app.put("/users/update", async (req, res) => {
 });
 
 app.delete("/users/delete", async (req, res) => {
+  console.log(req.cookies);
   const userData = req.body;
   const User = db.model("users", useSchema);
 
   try {
-    const presentUser = await User.findOne({ email: useData.email }, userData);
+    const presentUser = await User.findOne({ email: userData.email }, userData);
     if (!presentUser) {
       res.send({
         ok: false,
         message: "User with given email doesn't exist",
       });
+    } else {
+      await User.deleteOne({ email: userData.email }, userData);
+      res.send({
+        ok: true,
+        message: "User deleted successfully",
+      });
     }
-    await User.deleteOne({ email: email }, userData);
-    res.send({
-      ok: true,
-      message: "User deleted successfully",
-    });
   } catch (error) {
     res.send({
       ok: false,
       error: error,
     });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
 });
